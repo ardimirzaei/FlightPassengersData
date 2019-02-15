8ef5c985a0082d00ec3b0b5f9493425dd0ffafcc
ardimirzaei <45409241+ardimirzaei@users.noreply.github.com>
2019-02-16 10:48:10 +1100

    Update FlightPassengersManifest.R
    Make into dataframe with levels.

--- FlightPassengersManifest.R
+++ FlightPassengersManifest.R
@@ -1,4 +1,4 @@
-library(randomNames)
+require(randomNames)
 
 set.seed(1999)
 
@@ -19,7 +19,8 @@ Age<- as.integer(runif(100,2,75)) # Or antoher function of      abs(as.integer(r
 df$Age<-Age
 
 # Blood Types
-BloodTypes<-c(rep("O-positive", ceiling(Number_of_Passengers*(38/100))) , rep("O-negative", ceiling(Number_of_Passengers*(7/100))) , rep("A-positive", ceiling(Number_of_Passengers*(34/100))) , rep("A-negative", ceiling(Number_of_Passengers*(6/100))) , rep("B-positive", ceiling(Number_of_Passengers*(9/100))) , rep("B-negative", ceiling(Number_of_Passengers*(2/100))) , rep("AB-positive", ceiling(Number_of_Passengers*(3/100))) , rep("AB-negative", ceiling(Number_of_Passengers*(1/100))) )
+# Spread across rareness but i forgot the webstie, it was teh first on google
+BloodTypes<-c(rep("O-positive", ceiling(Number_of_Passengers*(0.38))) , rep("O-negative", ceiling(Number_of_Passengers*(0.07))) , rep("A-positive", ceiling(Number_of_Passengers*(0.34))) , rep("A-negative", ceiling(Number_of_Passengers*(0.06))) , rep("B-positive", ceiling(Number_of_Passengers*(0.09))) , rep("B-negative", ceiling(Number_of_Passengers*(0.02))) , rep("AB-positive", ceiling(Number_of_Passengers*(0.03))) , rep("AB-negative", ceiling(Number_of_Passengers*(0.01))) )
 df$BloodTypes<-sample(BloodTypes,Number_of_Passengers) # The blood type is spread based on rareness of the blood type
 
 # Height and Weight
@@ -40,8 +41,47 @@ AdultHeights<-round(runif(sum(is.na(df$Height)), 150, 200),1) # This is between 
 df$Height[is.na(df$Height)]<-AdultHeights
 
 # Beverages 
-
+# Made using the united airlines website https://www.united.com/web/en-US/content/travel/inflight/dining/beverages/default.aspx 
 Non_Alcoholic_Beverages<-c("Coca-Cola", "Coca-Cola Zero Sugar", "Diet Coke", "Sprite", "DASANI SPARKLING Lime", "DASANI Bottled Water", "Minute Maid Apple Juice", "Cranberry Apple Juice Cocktail", "Orange Juice", "Mott Tomato Juice", "Mr Mrs T Bloody Mary Mix", "Seagram Ginger Ale", "Seltzer Water", "Tonic Water", "Illy Dark Roast coffee regular ", "Illy Dark Roast coffee decaffeinated", "Hot tea")
 Alcoholic_Beverages<-c("Miller Lite", "Stella Artois", "Goose Island 312 Urban Wheat Ale", "Breckenridge Brewery Hop Peak IPA", "Redwood Vineyards Cabernet Sauvignon 2015", "Laboure Roi Chardonnay 2017 Vin de France", "Wheatley Craft Distilled Vodka", "Bacardi Superior Rum", "Dewar White Label Blended Scotch Whisky", "Jack Daniel Tennessee", "Baileys Irish Cream", "Bombay Sapphire Dry Gin", "Buffalo Trace Kentucky Straight Bourbon Whiskey", "Glenfarclas Single Malt Scotch Whisky")
 
 Beverages<-c(rep(Non_Alcoholic_Beverages,ceiling(Number_of_Passengers*0.66)), rep(Alcoholic_Beverages,ceiling(Number_of_Passengers*0.34)))
+df$Beverages<-sample(Beverages, Number_of_Passengers)
+df$Beverages[df$Age<18]<-sample(Non_Alcoholic_Beverages, sum(df$Age<18), replace=TRUE) # Makse sure the children are not hardcore alcholics
+
+df$BeveragesCount<-round(runif(Number_of_Passengers,1,5))
+ 
+AlcoBeverageIndex<-!is.na(match(df$Beverages, Alcoholic_Beverages))
+df$BeveragesAlcoholic<-AlcoBeverageIndex
+df$BeveragesAlcoholic<-ifelse(df$BeveragesAlcoholic, "Yes", "No")
+
+# Food
+
+# FoodOptions<-c("Chicken","Beef","Vegetarin","Fish","Special")
+# Spread is based on my guess. 
+Food<-c(rep("Chicken",ceiling(Number_of_Passengers*0.35)), rep("Beef",ceiling(Number_of_Passengers*0.35)), rep("Vegetarin",ceiling(Number_of_Passengers*0.15)),rep("Fish",ceiling(Number_of_Passengers*0.10)),rep("Special",ceiling(Number_of_Passengers*0.05)))
+df$Food<-sample(Food, Number_of_Passengers)
+
+
+# Factor Encoding
+Exportable_Dataframe<-data.frame(df)
+
+Exportable_Dataframe$gender<-factor(Exportable_Dataframe$gender)
+levels(Exportable_Dataframe$gender)<-c('Male','Female')
+
+Exportable_Dataframe$ethnicity<-factor(Exportable_Dataframe$ethnicity)
+levels(Exportable_Dataframe$ethnicity)<-c("American Indian or Native Alaskan", "Asian or Pacific Islander", "Black (not Hispanic)", "Hispanic", "White (not Hispanic)", "Middle-Eastern, Arabic")
+
+
+Exportable_Dataframe$BloodTypes<-factor(Exportable_Dataframe$BloodTypes)
+levels(Exportable_Dataframe$BloodTypes)<-c("O-positive" , "O-negative" , "A-positive" , "A-negative" , "B-positive" , "B-negative" , "AB-positive" , "AB-negative" )
+
+
+Exportable_Dataframe$Food<-factor(Exportable_Dataframe$Food)
+levels(Exportable_Dataframe$Food)<-c("Chicken","Beef","Vegetarin","Fish","Special")
+
+
+Exportable_Dataframe$BeveragesAlcoholic<-factor(Exportable_Dataframe$BeveragesAlcoholic)
+levels(Exportable_Dataframe$BeveragesAlcoholic)<-c("No","Yes")
+
+Exportable_Dataframe<- Exportable_Dataframe[order(Exportable_Dataframe$first_name),]
